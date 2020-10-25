c0c4667fa40cedf1740da3cc7ac0142b956616e9
Edu <nomail@nomail.com>
2020-10-25 22:22:33 +0100

    add two new parameters PRESERVE_DIR_STRUCTURE and DELETE_ORIGINAL_ON_SUCCESS

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,25 @@ find `pwd` -type f -name "*.mkv" | while read file; do
   m_filename=`basename "$file"`
   m_dirname=`dirname "$file"`
   m_filename_wo_ext="${m_filename%.*}"
-  m_newfilename="`pwd`/${m_filename_wo_ext}.mp4"
+
+  if [ -n "${PRESERVE_DIR_STRUCTURE:+x}" ]; then
+    output_dir="${m_dirname}"
+  else
+    output_dir="`pwd`"
+  fi
+  m_newfilename="${output_dir}/${m_filename_wo_ext}.mp4"
   avconv -loglevel error -y -i "$file" -c:v copy -c:a aac -strict experimental "$m_newfilename"
   if [ "$?" -eq 0 ]; then
     echo "Successfully processed: $file"
-    #/bin/rm -f "$file"
+    if [ -n "${DELETE_ORIGINAL_ON_SUCCESS:+x}" ]; then
+      /bin/rm -f "$file"
+      echo "original file deleted successfully"
+    else
+      echo "original file preserved"
+    fi
   fi
 
-  # Pause for 10 seconds before moving on to the next file
+  # Pause for 3 seconds before moving on to the next file
   sleep 3
 
 done
